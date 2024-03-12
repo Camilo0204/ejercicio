9c8f298e4e2c71bd0104a20e0cb6a5f305eae6f8
camilo Jaimes <olicaza02@gmail.com>
2024-03-11 19:46:01 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function EnviarMensaje() {
+    var nom = document.getElementById('nombre').value;
+    var ce = document.getElementById('ce').value;
+    var msj = document.getElementById('msj'.valueOf)    
+    alert('Apreciado' +nom+ ' hemos recibido su mensaje.  Nos comunicaremos a la direccion de email' +ce+ ' en los proximos dias');
+}
